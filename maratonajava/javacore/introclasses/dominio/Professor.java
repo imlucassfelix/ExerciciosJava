75a64361be0c70869387450f5ed8ae828c9ae5b8
Lucas Félix da Silva <lucassilvafelix533@gmail.com>
2026-04-19 20:20:47 -0300

    introduzi outra classe (professor)

---
+++ maratonajava/javacore/introclasses/dominio/Professor.java
@@ -0,0 +1,7 @@
+package maratonajava.javacore.introclasses.dominio;
+
+public class Professor {
+    public String nome = "Sanji";
+    public int idade;
+    public char sexo;
+}
---
+++ maratonajava/javacore/introclasses/test/ProfessorTest01.java
@@ -0,0 +1,14 @@
+package maratonajava.javacore.introclasses.test;
+
+import maratonajava.javacore.introclasses.dominio.Professor;
+
+public class ProfessorTest01 {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+        professor.nome = "Jiraya";
+        professor.sexo = 'M';
+        professor.idade = 52;
+
+        System.out.println("Nome: "+professor.nome+" ,Sexo: "+professor.sexo+" ,Idade: "+professor.idade);
+    }
+}
