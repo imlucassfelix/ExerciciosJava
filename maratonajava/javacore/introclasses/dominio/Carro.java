2436ad9140bc677428b26ee557731a0186ce7795
Lucas Félix da Silva <lucassilvafelix533@gmail.com>
2026-04-20 23:33:22 -0300

    exercicio classes

---
+++ maratonajava/javacore/introclasses/dominio/Carro.java
@@ -0,0 +1,9 @@
+package maratonajava.javacore.introclasses.dominio;
+
+public class Carro {
+    //Exercicio Crie uma classe carro com atributos: nome, modelo, ano
+    //Em seguida, crie dois objetos distintos e imprima seus valores
+    public String nome;
+    public String modelo;
+    public int ano;
+}
