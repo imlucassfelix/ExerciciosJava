dc08d0d6c3244ca1086a04f1f0e0de8109d6958c
Lucas Félix da Silva <lucassilvafelix533@gmail.com>
2026-04-23 13:32:44 -0300

    parametros

--- maratonajava/javacore/Bintrometodos/dominio/Calculadora.java
+++ maratonajava/javacore/Bintrometodos/dominio/Calculadora.java
@@ -3,10 +3,16 @@ package maratonajava.javacore.Bintrometodos.dominio;
 public class Calculadora {
 
     public void somaDoisNumeros(){
+
         System.out.println(10+10);
     }
 
     public void subtraiDoisNumeros2(){
+
         System.out.println(21-10);
+    }
+
+    public void multiplicaDoisNumeros(int num1, float num2){
+        System.out.println(num1*num2);
     }
 }
